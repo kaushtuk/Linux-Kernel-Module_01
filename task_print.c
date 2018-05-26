59505eb08e1d0c7bf25bb6f8f1ff10ec9d48314e
root <root@localhost.localdomain>
2018-05-27 00:18:40 +0530

    Chapter-3 Project-2 part-2

--- task_print.c
+++ task_print.c
@@ -3,15 +3,24 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+void dfs(struct task_struct * current_task)
+{
+    struct task_struct *task;
+    struct list_head *list;
+    list_for_each(list, &current_task->children) {
+        task = list_entry(list, struct task_struct, sibling);
+        /* task points to the next child in the list */
+        printk(KERN_INFO "%-16s:%10d:(%d)\n",task->comm,task->pid,task->real_parent->pid);
+        if(task != NULL)
+            dfs(task);
+    }
+}
 
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
-    struct task_struct *task;
-    for_each_process(task) {
-        printk(KERN_INFO "%-16s:%10d:%20ld\n",task->comm,task->pid,task->state);
-        /* on each iteration task points to the next task */
-    }
+    printk(KERN_INFO "%-16s:%10d\n",init_task.comm,init_task.pid);
+    dfs(&init_task);
     return 0;
 }
 
